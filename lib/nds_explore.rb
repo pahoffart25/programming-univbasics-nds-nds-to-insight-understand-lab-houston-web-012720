01b3685611809c9c56fa28945628af379800d47e
Paul Hoffart <pahoffart25@gmail.com>
2020-01-06 16:54:43 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,7 +12,7 @@ def print_first_directors_movie_titles
 directors_database.each do |name, info|
 info[:movies].each do |title|
   if name == "Stephen Spielberg"
-    puts title[:movies][:title]
+    puts info[:title]
   end
     end
   end
